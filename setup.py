9d7d04bd698c97357276b28dc54f77d930da11c1
Damian <surrerstry@gmail.com>
2018-05-05 23:36:55 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-	name = 'octopus',
-	packages = ['octopus'],
-	version = '0.41',
+	name = 'distributed_containers',
+	packages = ['distributed_containers'],
+	version = '0.42',
 	description = "Conceptual library written for pypy-stm that target is to get more from multithreading possibilities of pypy-stm.",
 	author = 'Damian Medrala',
 	author_email = 'surrerstry@gmail.com',
-	url = 'https://github.com/Surrerstry/octopus',
-	download_url = 'https://github.com/Surrerstry/octopus/archive/master.zip',
+	url = 'https://github.com/Surrerstry/distributed_containers',
+	download_url = 'https://github.com/Surrerstry/distributed_containers/archive/master.zip',
 	keywords = ['pypy-stm', 'threading'],
 	classifiers = [],
 	)
